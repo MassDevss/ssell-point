fd6cc03f01211605474c0e7f88f1b99067c0806b
Manuel Samaniego <samaniegoserranom@gmail.com>
2022-11-17 13:02:44 -0700

    gran avance creacion de componente producto, falta terminar funcionabilidad

--- Ui/main.js
+++ Ui/main.js
@@ -3,6 +3,13 @@
 
 const { app, BrowserWindow } = require('electron')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('electron-reload')(__dirname, {
+    
+  })
+}
+
+
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
---
+++ server/puntoTiburon/src/test/java/com/pvntProject/puntoTiburon/PuntoTiburonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvntProject.puntoTiburon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoTiburonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
