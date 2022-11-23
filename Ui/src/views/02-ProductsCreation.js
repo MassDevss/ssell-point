5c2fb1d3955ec58b57b43d0760e91a1495773dc1
ManuelsSaNt <95022226+ManuelsSaNt@users.noreply.github.com>
2022-11-23 13:25:29 -0700

    arreglos de danny a

---
+++ Ui/src/views/02-ProductsCreation.js
@@ -0,0 +1,22 @@
+// tiene que haber una peticion para cargar todos los productos que hay
+// manual por lo pronto
+
+const tiburon = new Product("Tiburon", 125, "BurgersLoads");
+tiburon.generarNew();
+allProduct.push(tiburon);
+
+const chipocluda = new Product("chipocluda", 125, "BurgersLoads");
+chipocluda.generarNew();
+allProduct.push(chipocluda);
+
+const mexicana = new Product("Mexicana", 125, "BurgersLoads");
+mexicana.generarNew();
+allProduct.push(mexicana);
+
+const chesseBurger = new Product("chesseBurger", 125, "BurgersLoads");
+chesseBurger.generarNew();
+allProduct.push(chesseBurger);
+
+const chickenFried = new Product("chickenFried", 125, "BurgersLoads");
+chickenFried.generarNew();
+allProduct.push(chickenFried);
