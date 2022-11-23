8f1374ea62ae8d4a147d2e609257c71e1340eb78
ManuelsSaNt <95022226+ManuelsSaNt@users.noreply.github.com>
2022-11-23 08:40:27 -0700

    mejora del arbol de directorios

--- Ui/src/views/main-view-index.js
+++ Ui/src/views/main-view-index.js
@@ -1,6 +1,7 @@
+
 /*
 
-  nombre:  =>   obviamente es el nombre del producto
+  nombre:  =>   obviamente es el nombre del producto 
   precio: => igualmente obvio es el precio del producto
 
   tipo: => ATENCION::::::::::::::::::::;
@@ -11,7 +12,7 @@
   ya que con este campo se decide donde se insertaran
   ______________________________________
   |                   |
-  |Hamburguesa        |   "BurgersLoads"
+  |Hamburguesa        |   "BurgersLoads" 
   |                   |
   |Pollo              |   "PolloLoads"
   |                   |
@@ -24,80 +25,86 @@
   |Extras             |
   |___________________|___________________
 */
-var allProduct = [];
-var Product = /** @class */ (function () {
-    function Product(nombre, precio, tipo) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.tipo = tipo;
-    }
-    Product.prototype.buttonFunction = function (boton, action) {
-        var _this = this;
-        boton.addEventListener('click', function () {
-            //@ts-ignore
-            var campo = document.querySelector("#cantidad-".concat(_this.nombre));
-            if (action === "+") {
-                //@ts-ignore
-                campo.value++;
-            }
-            else if (action === "-") {
-                //@ts-ignore
-                if (campo.value > 0) {
-                    //@ts-ignore
-                    campo.value--;
-                }
-            }
-        });
-    };
-    Product.prototype.generarNew = function () {
-        // consolidacion de la estuctura
-        var productsContainer = document.getElementById(this.tipo);
-        var divToCard = document.createElement('div');
-        divToCard.className = "col-md-6 card";
-        var titleProduct = document.createElement('h4');
-        titleProduct.innerHTML = this.nombre;
-        var inpCantidad = document.createElement('input');
+let allProduct = [];
+
+class Product {
+  nombre;
+  precio;
+  tipo;
+  constructor(nombre, precio, tipo) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.tipo   = tipo;
+  }
+
+  buttonFunction(boton, action) {
+    boton.addEventListener('click', ()=>{
+      //@ts-ignore
+      let campo = document.querySelector(`#cantidad-${this.nombre}`);
+      if (action === "+"){
         //@ts-ignore
-        inpCantidad.style = "text-align: center;";
-        inpCantidad.className = "form-control";
-        inpCantidad.id = 'cantidad-' + this.nombre;
-        var divButtons = document.createElement('div');
-        var btnMas = document.createElement('button');
-        btnMas.className = "col-6 btn btn-success";
+        campo.value++;
+      }
+      else if (action === "-"){
         //@ts-ignore
-        btnMas.style = "border-radius: 0px 0px 5px 0px;";
-        btnMas.innerHTML = "+1";
-        var btnMenos = document.createElement('button');
-        btnMenos.className = "col-6 btn btn-danger";
-        //@ts-ignore
-        btnMenos.style = "border-radius: 0px 0px 0px 5px;";
-        btnMenos.innerHTML = "-1";
-        divToCard.appendChild(titleProduct);
-        divToCard.appendChild(inpCantidad);
-        divButtons.appendChild(btnMenos);
-        divButtons.appendChild(btnMas);
-        divToCard.appendChild(divButtons);
-        //@ts-ignore
-        productsContainer.append(divToCard);
-        this.buttonFunction(btnMas, "+");
-        this.buttonFunction(btnMenos, "-");
-    };
-    return Product;
-}());
-// tiene que haber una peticion para cargar todos los productos que hay
-// manual por lo pronto
-var tibu = new Product("Tiburon", 125, "BurgersLoads");
-tibu.generarNew();
-allProduct.push(tibu);
-var chipo = new Product("chipocluda", 125, "BurgersLoads");
-chipo.generarNew();
-allProduct.push(chipo);
+        if (campo.value > 0){
+          //@ts-ignore
+          campo.value--;
+        }
+      }
+    })
+  }
+
+  generarNew() {
+
+  // consolidacion de la estuctura
+  const productsContainer = document.getElementById(this.tipo);
+
+  let divToCard = document.createElement('div');
+  divToCard.className = "col-md-6 card";
+  let titleProduct = document.createElement('h4');
+  titleProduct.innerHTML = this.nombre;
+  let inpCantidad = document.createElement('input');
+  //@ts-ignore
+  inpCantidad.style = "text-align: center;"
+  inpCantidad.className = "form-control";
+  inpCantidad.id = 'cantidad-' + this.nombre;
+  let divButtons = document.createElement('div')
+  let btnMas = document.createElement('button')
+  btnMas.className = "col-6 btn btn-success";
+  //@ts-ignore
+  btnMas.style = "border-radius: 0px 0px 5px 0px;"
+  btnMas.innerHTML = "+1";
+  let btnMenos = document.createElement('button');
+  btnMenos.className = "col-6 btn btn-danger";
+  //@ts-ignore
+  btnMenos.style = "border-radius: 0px 0px 0px 5px;"
+  btnMenos.innerHTML = "-1";
+
+  divToCard.appendChild(titleProduct);
+  divToCard.appendChild(inpCantidad);
+
+  divButtons.appendChild(btnMenos);
+  divButtons.appendChild(btnMas);
+
+  divToCard.appendChild(divButtons);
+
+  //@ts-ignore
+  productsContainer.append(divToCard);
+  this.buttonFunction(btnMas, "+");
+  this.buttonFunction(btnMenos, "-");
+  }
+
+}
+
+
 // funcion para sacar la suma total de productos y de costo
 function plusAllProducts() {
-    var sumaTotal = 0;
-    var cantidadProductos = 0;
-    for (var i = 0; i < allProduct.length; i++) {
-        sumaTotal += allProduct[i].precio;
-        cantidadProductos++;
-    }
+  let sumaTotal = 0;
+  let cantidadProductos = 0;
+  for(let i = 0; i < allProduct.length; i++){
+    sumaTotal += allProduct[i].precio;
+    cantidadProductos++;
+  }
 }
+
