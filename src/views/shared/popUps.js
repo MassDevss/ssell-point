9fb42efe8093e59f705df57d1fe98ff50a9a5d23
ManuelsSaNt <95022226+ManuelsSaNt@users.noreply.github.com>
2023-07-26 02:42:54 -0700

    advance in module modals

--- src/views/shared/popUps.js
+++ src/views/shared/popUps.js
@@ -1,7 +1,45 @@
 
+const body = document.getElementsByTagName('body');
 
 
+function messageModal(isDanger){
+	// if is danger is good show and icon or similar
+
+	const modalBase = document.createElement('DIV');
+	modalBase.className = 'base-modal';
+	
+	const modalBody = document.createElement('ARTICLE');
+	modalBody.className = 'body-modal';
+
+}
 
 
+/**
+ * 
+ * @param {string} question -> question to show in modal
+ * @param {function} onCancel 
+ * @param {function} onApprove 
+ */
+function questionModal(question, onCancel, onApprove) {
+
+	const modalBase = document.createElement('DIV');
+	modalBase.className = 'base-modal';
+	
+	const modalBody = document.createElement('ARTICLE');
+	modalBody.className = 'body-modal';
+
+
+	const buttonCancel = document.createElement('BUTTON');
+	const buttonApporve = document.createElement('BUTTON');
+	
+	buttonCancel.addEventListener('click', () => {
+		onCancel();
+	});
+
+	buttonApporve.addEventListener('click', () => {
+		onApprove();
+	});
+
+}
 
 
